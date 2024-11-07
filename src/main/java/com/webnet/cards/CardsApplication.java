2bb3dfe2833371188f7185abda90fb444a3d7476
abammeke <abammeke222@gmail.com>
2024-11-06 21:18:35 -0600

    1004 - Initial commit

---
+++ src/main/java/com/webnet/cards/CardsApplication.java
@@ -0,0 +1,18 @@
+package com.webnet.cards;
+
+import com.webnet.cards.dto.CardsContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
+@EnableFeignClients
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webnet/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webnet.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
